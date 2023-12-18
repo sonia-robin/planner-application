0b1155e59c5b09feb5098e3112d3dc8d8baebbb8
unknown <cervinkova.sonik@gmail.com>
2023-12-18 19:13:36 +0000

    local storage added

--- script.js
+++ script.js
@@ -6,16 +6,12 @@ function currentDayTime (){
 // Update time every second
 setInterval(currentDayTime, 1000)
 
-// Display the current day at the top of the calender when a user opens the planner.
-// var today = dayjs().format("dddd, MMMM Do");
-// $("#currentDay").text(today);
-
 var currentHour = dayjs().format("HH");
 console.log(currentHour);
 
 // Color-code each timeblock based on past, present, and future
 $(".time-block").each(function(){
-    var timeId = this.id
+    var timeId = this.id;
     console.log(timeId);
     if(currentHour > timeId){
         $(this).children("textarea").addClass("past");
@@ -27,3 +23,20 @@ $(".time-block").each(function(){
         $(this).children("textarea").addClass("future");
     }
 });
+
+// Save the event in local storage when the save button is clicked in that timeblock.
+// Add event listener to the save button
+$(".saveBtn").on("click", function(){
+    var timeBlockId = $(this).parent().attr('id');
+    console.log(timeBlockId);
+    var textEntry = $(this).siblings(".description").val();
+    console.log(textEntry);
+    localStorage.setItem(timeBlockId, textEntry);
+})
+
+// Read data from local storage and persist events between refreshes of a page
+$(".time-block").each(function(){
+    var textEntry = localStorage.getItem(this.id);
+    console.log(textEntry);
+    $(this).children(".description").val(textEntry);
+})
